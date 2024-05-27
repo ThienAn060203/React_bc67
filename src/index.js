7d6072f04c732760900896e37221d867a6d08250
Thienan060203 <phanphamthienan2003@gmail.com>
2024-05-27 14:45:35 +0700

    start project react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//tạo root trên thẻ div #root
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render("hello bc67");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//jsx
+root.render(<div>hello bc67</div>);
